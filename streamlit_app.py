6193f06f93f101cea2fe445820f82bf8949fac53
pmespresso <yjkimjunior@gmail.com>
2023-11-21 13:41:57 +0800

    refactor

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,73 +17,106 @@ from langchain.memory import ConversationBufferMemory
 # from langchain.document_transformers import EmbeddingsRedundantFilter
 from utils.stream_handler import StreamHandler
 
+
+# Load environment variables
 load_dotenv()
 
-st.set_page_config(page_title="🐇 WhatsUpDoc")
-st.title('🐇 WhatsUpDoc')
-st.sidebar.title('🐇 WhatsUpDoc')
+def setup_streamlit_page():
+    st.set_page_config(page_title="🐇 WhatsUpDoc")
+    st.title('🐇 WhatsUpDoc')
+    st.sidebar.title('🐇 WhatsUpDoc')
+    st.sidebar.markdown("""
+    WhatsUpDoc is a **search engine** for your tech stack's documentation and broader knowledge base. 
+    It uses **OpenAI** to understand your questions and **Langchain** to search your tech stack's documentation and broader knowledge base.
+    """)
 
-st.sidebar.markdown('WhatsUpDoc is a **search engine** for your tech stack\'s documentation and broader knowledge base. It uses **OpenAI** to understand your questions and **Langchain** to search your tech stack\'s documentation and broader knowledge base.')
 
-tech_stack_table_name = {
-  'Tailwind CSS': 'tailwind_documents',
-  'NextJS': 'nextjs_documents',
-  'Stripe SDK': 'stripe_documents',
-  'Langchain SDK': 'langchain_documents',
-  'Streamlit SDK': 'streamlit_documents',
-}
+def initialize_globals():
+    openai_api_key = os.getenv("OPENAI_API_KEY") or st.sidebar.text_input('OpenAI API Key')
+    supabase_url = os.getenv("SUPABASE_URL")
+    supabase_key = os.getenv("SUPABASE_SERVICE_KEY")
+    supabase = create_client(supabase_url, supabase_key)
+    embeddings = OpenAIEmbeddings()
+    return openai_api_key, supabase, embeddings
 
-with st.sidebar:
-  st.markdown('This is a **demo** of the WhatsUpDoc app. Please enter your OpenAI API key below to get started.')
-  st.markdown('The app is currently in **alpha** and is under active development.')
 
-  st.divider()
+def configure_sidebar():
+    with st.sidebar:
+        st.markdown('This is a **demo** of the WhatsUpDoc app. Please enter your OpenAI API key below to get started.')
+        st.markdown('The app is currently in **alpha** and is under active development.')
+        st.divider()
 
-  tech_stack = st.selectbox(label="Choose Tech", help='Search the Documentation and Broader Knowledge Base of Your Tech Stack. ', options=('Tailwind CSS', 'NextJS', 'Stripe SDK', 'Langchain SDK', 'Supabase SDK'), label_visibility='visible')
+        tech_stack_options = {
+            'Tailwind CSS': 'tailwind_documents',
+            'NextJS': 'nextjs_documents',
+            'Stripe SDK': 'stripe_documents',
+            'Langchain SDK': 'langchain_documents',
+            'Streamlit SDK': 'streamlit_documents',
+        }
+        tech_stack = st.selectbox(
+            label="Choose Tech", 
+            help='Search the Documentation and Broader Knowledge Base of Your Tech Stack.',
+            options=list(tech_stack_options.keys()), 
+            label_visibility='visible'
+        )
 
-  st.divider()
+        st.divider()
+        with st.form('waitlist_form'):
+            st.text_input('Join the waitlist for early access to the app:', placeholder='e.g. harrisonchase@gmail.com')
+            st.form_submit_button("Submit")
 
-  st.text_input('Join the waitlist for early access to the app:', placeholder='e.g. kellyslater@gmail.com')
+    return tech_stack_options[tech_stack]
 
-openai_api_key = os.environ.get("OPENAI_API_KEY") or st.sidebar.text_input('OpenAI API Key')
+def generate_response(input_text, chat_box):
 
-supabase_url = os.environ.get("SUPABASE_URL")
-supabase_key = os.environ.get("SUPABASE_SERVICE_KEY")
-supabase: Client = create_client(supabase_url, supabase_key)
+  compression_retriever = get_retriever()
+  qa_chain = get_qa_chain(compression_retriever, chat_box)
 
-embeddings = OpenAIEmbeddings()
+  result = qa_chain({ "question": input_text })
 
-def get_vectorstore():
-  vector_store = SupabaseVectorStore(
-    embedding=embeddings,
-    client=supabase,
-    chunk_size=100,
-    table_name=tech_stack_table_name[tech_stack],
-    query_name=f"match_{tech_stack_table_name[tech_stack]}",
-  )
+  st.markdown(result['answer'])
 
-  return vector_store
 
-def get_retriever(vector_store: SupabaseVectorStore):
-  reordering = LongContextReorder()
-  relevant_filter = EmbeddingsFilter(embeddings=embeddings, similarity_threshold=0.76)
+  if result['source_documents']:
+    for document in result['source_documents']:
+      st.markdown(document['page_content'])
 
-  # TODO: Maybe add these back in later, or other Filters
-  # splitter = CharacterTextSplitter(chunk_size=300, chunk_overlap=0, separator=". ")
-  # redundant_filter = EmbeddingsRedundantFilter(embeddings=embeddings)
+def run_main_application(tech_stack, openai_api_key):
+    st.title(tech_stack)
+    with st.form('my_form'):
+        text = st.text_area('Enter text:', 'How do I set a gradient from teal to blue to purple to a full page background?')
+        submitted = st.form_submit_button('Submit')
+        if not openai_api_key.startswith('sk-'):
+            st.warning('Please enter your OpenAI API key!', icon='⚠️')
+        if submitted and openai_api_key.startswith('sk-'):
+            chat_box = st.empty()
+            generate_response(text, chat_box)
+ 
 
-  pipeline_compressor = DocumentCompressorPipeline(
-      transformers=[
-          # splitter,
-          # redundant_filter,
-          relevant_filter,
-          reordering
-      ]
-  )
 
-  compression_retriever = ContextualCompressionRetriever(base_compressor=pipeline_compressor, base_retriever=vector_store.as_retriever())
+def get_retriever():
+    vectorstore = SupabaseVectorStore(
+        embedding=embeddings,
+        client=supabase_client,
+        chunk_size=100,
+        table_name=tech_stack,
+        query_name=f"match_{tech_stack}",
+    )
 
-  return compression_retriever
+    relevant_filter = EmbeddingsFilter(embeddings=embeddings, similarity_threshold=0.76)
+    pipeline_compressor = DocumentCompressorPipeline(
+        transformers=[
+            relevant_filter,
+            LongContextReorder()
+        ]
+    )
+
+    compression_retriever = ContextualCompressionRetriever(
+        base_compressor=pipeline_compressor, 
+        base_retriever=vectorstore.as_retriever()
+    )
+    return compression_retriever
+
 
 def get_qa_chain(compression_retriever: ContextualCompressionRetriever, chat_box):
   # Instantiate ConversationBufferMemory
@@ -96,28 +129,14 @@ def get_qa_chain(compression_retriever: ContextualCompressionRetriever, chat_box
 
   return qa
 
-def generate_response(input_text, chat_box):
-  vectorstore = get_vectorstore()
-  compression_retriever = get_retriever(vectorstore)
-  qa_chain = get_qa_chain(compression_retriever, chat_box)
+# Setup Streamlit page configuration
+setup_streamlit_page()
 
-  result = qa_chain({ "question": input_text })
+# Initialize global variables
+openai_api_key, supabase_client, embeddings = initialize_globals()
 
-  st.markdown(result['answer'])
+# Sidebar configuration
+tech_stack = configure_sidebar()
 
-
-  if result['source_documents']:
-    for document in result['source_documents']:
-      st.markdown(document['page_content'])
-
-
-st.title(tech_stack)
-
-with st.form('my_form'):
-  text = st.text_area('Enter text:', 'How do I set a gradient from teal to blue to purple to a full page background?')
-  submitted = st.form_submit_button('Submit')
-  if not openai_api_key.startswith('sk-'):
-    st.warning('Please enter your OpenAI API key!', icon='⚠')
-  if submitted and openai_api_key.startswith('sk-'):
-    chat_box = st.empty()
-    generate_response(text, chat_box)
+# Main application
+run_main_application(tech_stack, openai_api_key)
