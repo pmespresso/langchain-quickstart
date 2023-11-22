51ed1c3fe83b465e90e80a6250e16d2dbdeb662e
pmespresso <yjkimjunior@gmail.com>
2023-11-22 11:48:10 +0800

    fix: checking of keys and keep session state as long as no refresh

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,9 +18,14 @@ from langchain.memory import ConversationBufferMemory
 # from langchain.document_transformers import EmbeddingsRedundantFilter
 from utils.stream_handler import StreamHandler
 
-
 # Load environment variables
 load_dotenv()
+
+# Initialize the session state for verification status
+if 'verified' not in st.session_state:
+    st.session_state.verified = False
+if 'openai_api_key' not in st.session_state:
+    st.session_state.openai_api_key = None
 
 # Function to call the API for license key verification
 def verify_license_key(license_key):
@@ -29,11 +34,15 @@ def verify_license_key(license_key):
     data = {"license_key": license_key}
     response = requests.post(url, headers=headers, data=data)
     # Assuming a successful response indicates a valid license key
+    if response.ok:
+        st.session_state.verified = True
+    else:
+        st.session_state.verified = False
     return response.ok
 
 def setup_streamlit_page():
-    st.set_page_config(page_title="🐇 WhatsUpDoc")
-    st.sidebar.title('🐇 WhatsUpDoc')
+    st.set_page_config(page_title="WhatsUpDoc 🐇")
+    st.sidebar.title('WhatsUpDoc 🐇')
     st.sidebar.markdown("""
     WhatsUpDoc is a **search engine** for your tech stack's documentation and broader knowledge base. 
     It uses **OpenAI** to understand your questions and **Langchain** to search your tech stack's documentation and broader knowledge base.
@@ -41,12 +50,13 @@ def setup_streamlit_page():
 
 
 def initialize_globals():
-    openai_api_key = os.getenv("OPENAI_API_KEY")
+    openai_api_key = os.getenv("OPENAI_API_KEY") or st.session_state.openai_api_key
+    verified = st.session_state.verified
     supabase_url = os.getenv("SUPABASE_URL")
     supabase_key = os.getenv("SUPABASE_SERVICE_KEY")
     supabase = create_client(supabase_url, supabase_key)
     embeddings = OpenAIEmbeddings()
-    return openai_api_key, supabase, embeddings
+    return openai_api_key, supabase, embeddings, verified
 
 
 def configure_sidebar():
@@ -70,75 +80,46 @@ def configure_sidebar():
 
         st.divider()
 
-        # Form for license key input
-        with st.form("license_key_form"):
-            license_key = st.text_input("License Key", "")
-            submitted = st.form_submit_button("Verify")
+        # Display the verification message if the license key is already verified
+        if st.session_state.get('verified', False):
+            st.success("License Key Verified")
+        else:
+          # Form for license key input
+          with st.form("license_key_form"):
+              license_key = st.text_input("License Key", "")
+              submitted = st.form_submit_button("Verify")
 
-            if submitted:
-                if verify_license_key(license_key):
-                    st.success("Verified License Key")
-                    verified = True
-                else:
-                    st.error("Invalid License Key")
-                    verified = False
-            else:
-                verified = False
-        
+              if submitted:
+                  if verify_license_key(license_key):
+                      st.session_state.verified = True
+                      st.success("License Key Verified")
+                  else:
+                      st.session_state.verified = False
+                      st.error("Invalid License Key")
+                      
         # Display the button for the deal only if the license key is verified
-        if not verified:
+        if not st.session_state.verified:
             deal_link = 'https://whatsupdoc.lemonsqueezy.com/checkout/buy/7a80a616-ef60-4fe6-9ff0-d67acacc8ab0'
             st.markdown(f"<a href='{deal_link}' target='_blank'><button style='background-color: #FF4B4B; color: white; border: none; padding: 10px 20px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer;'>👉 Get Limited Lifetime Deal $29.99</button></a>", unsafe_allow_html=True)
 
+        # If OpenAI API Key is not yet set, display the form to set it
+        if not st.session_state.openai_api_key:
+            with st.form("openai_api_key_form"):
+              st.markdown('Please enter your OpenAI API key below to get started.')
+              openai_api_key = st.text_input("OpenAI API Key", "")
+              submit_openai_key_button = st.form_submit_button("Submit")
 
-        if not openai_api_key:
-          st.divider()
-          st.markdown('This is a **demo** of the WhatsUpDoc app. Please enter your OpenAI API key below to get started.')
-          st.sidebar.text_input('OpenAI API Key')
+              if submit_openai_key_button:
+                  st.session_state.openai_api_key = openai_api_key
+                  st.success("OpenAI API Key is Set")
+              else:
+                  st.session_state.openai_api_key = None
+                  st.error("OpenAI API Key is Not Set")
+        else:
+          st.success("OpenAI API Key is Set")
+
 
     return tech_stack_options[tech_stack]
-
-def generate_response(input_text, chat_box):
-  compression_retriever = get_retriever()
-  qa_chain = get_qa_chain(compression_retriever, chat_box)
-
-  result = qa_chain({ "question": input_text })
-
-  st.markdown(result['answer'])
-
-  print (result.keys())
-
-  source_documents = result.get('source_documents')
-
-  if source_documents:
-    st.header("Sources")
-    for document in source_documents:
-        if document:
-            # Access attributes directly
-            # page_content = document.page_content
-            metadata = document.metadata
-
-            # If you need to access specific metadata attributes
-            if metadata:
-                # location = metadata.get('loc', 'No location provided')
-                source = metadata.get('source', 'No source provided')
-
-                # Use location and source as needed
-                # st.markdown(f"Location: {location}")
-                st.markdown(f"{source}")
-
-def run_main_application(tech_stack, openai_api_key):
-    st.title(tech_stack)
-    with st.form('my_form'):
-        text = st.text_area('Enter text:', 'How do I set a gradient from teal to blue to purple to a full page background?')
-        submitted = st.form_submit_button('Submit')
-        if not openai_api_key.startswith('sk-'):
-            st.warning('Please enter your OpenAI API key!', icon='⚠️')
-        if submitted and openai_api_key.startswith('sk-'):
-            chat_box = st.empty()
-            generate_response(text, chat_box)
- 
-
 
 def get_retriever():
     vectorstore = SupabaseVectorStore(
@@ -168,21 +149,63 @@ def get_qa_chain(compression_retriever: ContextualCompressionRetriever, chat_box
   # Instantiate ConversationBufferMemory
   memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True, output_key='answer')
 
-  stream_handler = StreamHandler(chat_box, display_method='write')
+  stream_handler = StreamHandler(chat_box, display_method='markdown')
   llm = ChatOpenAI(temperature=0.3, model='gpt-4-1106-preview', streaming=True, callbacks=[stream_handler])
 
   qa = ConversationalRetrievalChain.from_llm(llm=llm, retriever=compression_retriever, memory=memory, return_source_documents=True)
 
   return qa
 
+def generate_response(input_text, chat_box):
+  compression_retriever = get_retriever()
+  qa_chain = get_qa_chain(compression_retriever, chat_box)
+
+  result = qa_chain({ "question": input_text })
+
+  st.markdown(result['answer'])
+
+  print (result.keys())
+
+  source_documents = result.get('source_documents')
+
+  if source_documents:
+    st.header("Sources")
+    for document in source_documents:
+        if document:
+            # page_content = document.page_content
+            metadata = document.metadata
+
+            # If you need to access specific metadata attributes
+            if metadata:
+                source = metadata.get('source', 'No source provided')
+                st.markdown(f"{source}")
+
+def run_main_application(tech_stack, verified, openai_api_key):
+    st.title(tech_stack)
+
+    # Check if the license key is verified
+    if not verified:
+        st.warning("Please verify your license key before using the app!", icon="⚠️")
+        st.markdown("Go to the sidebar to enter and verify your license key.")
+    else:
+      with st.form('main'):
+          text = st.text_area('Enter text:', 'How do I set a gradient from teal to blue to purple to a full page background?')
+          submitted = st.form_submit_button('Submit')
+          if not openai_api_key or not openai_api_key.startswith('sk-'):
+              st.warning('Please enter your OpenAI API key!', icon='⚠️')
+          if submitted and openai_api_key and openai_api_key.startswith('sk-'):
+              chat_box = st.empty()
+              generate_response(text, chat_box)
+
+
 # Setup Streamlit page configuration
 setup_streamlit_page()
 
 # Initialize global variables
-openai_api_key, supabase_client, embeddings = initialize_globals()
+openai_api_key, supabase_client, embeddings, verified = initialize_globals()
 
 # Sidebar configuration
 tech_stack = configure_sidebar()
 
 # Main application
-run_main_application(tech_stack, openai_api_key)
+run_main_application(tech_stack, verified, openai_api_key)
